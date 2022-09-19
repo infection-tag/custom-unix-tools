ce12dd5f5b496de28155906583307771415ff467
Nishant <qweenjon@gmail.com>
2022-09-19 11:06:01 -0700

    added meta script

---
+++ src/meta.sh
@@ -0,0 +1,9 @@
+echo /------------------------------------------------------------------
+du -ch && echo B
+pwd
+
+
+# check if directory is git initalized
+[ -d .git ] && echo Git initalized || git rev-parse --git-dir > /dev/null 2>&1
+
+echo ------------------------------------------------------------------/
